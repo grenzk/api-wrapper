edae2f75468ec1921d7006be86400eca118d9d0d
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-24 16:47:04 +0800

    Create API Wrapper

--- app.rb
+++ app.rb
@@ -3,13 +3,24 @@ require_relative 'api/youtube_api/v3/client'
 
 Dotenv.load('credentials.env')
 
-@client = YouTube::Client.new(api_key: ENV['YOUTUBE_API_KEY'])
+client = YouTube::Client.new(api_key: ENV['YOUTUBE_API_KEY'])
 
-@client.connection.get(
+client.connection.get(
+  'playlists',
+  {
+    part: 'snippet,contentDetails',
+    mine: true,
+    maxResults: 25,
+    key: "#{client.api_key}"
+  },
+  { Authorization: "Bearer #{ENV['YOUTUBE_ACCESS_TOKEN']}" }
+)
+
+client.connection.get(
   'videos',
   {
     id: '7lCDEYXw3mM',
-    key: "#{@client.api_key}",
+    key: "#{client.api_key}",
     part: 'snippet,contentDetails,statistics,status'
   }
 )
